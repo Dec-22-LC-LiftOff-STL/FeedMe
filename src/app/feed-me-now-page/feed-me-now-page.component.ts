a133c746ba73bdff97f9ee8abb6e5001114ddb40
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-12-14 14:57:00 -0600

    Workign on array

--- src/app/feed-me-now-page/feed-me-now-page.component.ts
+++ src/app/feed-me-now-page/feed-me-now-page.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 
+import {NgForm} from '@angular/forms';
+
 @Component({
   selector: 'app-feed-me-now-page',
   templateUrl: './feed-me-now-page.component.html',
   styleUrls: ['./feed-me-now-page.component.css']
 })
 export class FeedMeNowPageComponent {
-
+ 
+    fastFoods = [
+     {name: "Taco Bell"}];
+     addNewFastFood() {
+       this.fastFoods.push(fastFoodName)
+     }
+ 
+ }
 }
-const fastFood = ["Taco Bell"]
-fastFood.push 
+
+
