53ec4608af96598f7ed783938ffdae6665a7bfce
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-12-16 18:55:24 -0600

    cleaned up code some and im doen for the night

--- src/app/feed-me-now-page/feed-me-now-page.component.ts
+++ src/app/feed-me-now-page/feed-me-now-page.component.ts
@@ -1,59 +1,39 @@
 import { Component } from '@angular/core';
 
-
 import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-feed-me-now-page',
   templateUrl: './feed-me-now-page.component.html',
-  styleUrls: ['./feed-me-now-page.component.css']
+  styleUrls: ['./feed-me-now-page.component.css'],
 })
 export class FeedMeNowPageComponent {
+  orderFoods: string[] = [];
 
-  orderFoods: string[] = [
-   ];
+  onHandFoods: string[] = [];
 
-  onHandFoods: string[] = [
-    
-  ]
-
-  randomOnHandFood: string = ''
-  randomOrderFood: string = ''
-
-
-  addNewFastFood = (): void => {
-    this.orderFoods.push("fastFoodName")
-  }
-  //  fastFood="";
+  randomOnHandFood: string = '';
+  randomOrderFood: string = '';
 
   randomizer = (array: string[]): string => {
-    return array[Math.floor(Math.random() * array.length)]
-  }
+    return array[Math.floor(Math.random() * array.length)];
+  };
 
   randomizeOnHandFood = (): void => {
-    this.randomOnHandFood = this.randomizer(this.onHandFoods)
-  }
+    this.randomOnHandFood = this.randomizer(this.onHandFoods);
+  };
 
   randomizeOrderFood = (): void => {
-    this.randomOrderFood = this.randomizer(this.orderFoods)
+    this.randomOrderFood = this.randomizer(this.orderFoods);
+  };
+
+  addNewOrder(value: string) {
+    this.orderFoods.push(value);
+    console.log(this.orderFoods);
   }
-//AddSnackby get element
-// addNewSnack(){
-//   this.onHandFoods.push(document.getElementById("snackInput"))
-// }
 
-addNewOrder(value: string) {
-  this.orderFoods.push(value)
-  console.log(this.orderFoods);
+  addNewSnack(value: string) {
+    this.onHandFoods.push(value);
+    console.log(this.onHandFoods);
+  }
 }
-
-
-
-addNewSnack(value: string) {
-  this.onHandFoods.push(value)
-  console.log(this.onHandFoods);
-}
-}
-
-
-
