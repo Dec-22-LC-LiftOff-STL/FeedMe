6832dfb3f7999af297f53851f8e6ccd0aa29a0e7
KazMaune <kikainvr@gmail.com>
2022-12-20 14:36:37 -0600

    Finished logic for adding a new column

--- src/app/feed-me-now-page/feed-me-now-page.component.ts
+++ src/app/feed-me-now-page/feed-me-now-page.component.ts
@@ -1,18 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+interface Column {
+  items: string[];
 
-import { NgForm } from '@angular/forms';
+  columnKey: string;
+
+  columnTitle: string;
+}
 
 @Component({
   selector: 'app-feed-me-now-page',
   templateUrl: './feed-me-now-page.component.html',
   styleUrls: ['./feed-me-now-page.component.css'],
 })
-export class FeedMeNowPageComponent {
-    items1: string[] = [""];
+export class FeedMeNowPageComponent implements OnInit {
+    columns: Column[] = [{
+      items: [""], 
+      columnKey: "items1",
+      columnTitle: "Snacks"
+    }, {
+      items: [""],
+      columnKey: "items2",
+      columnTitle: "Take Out"
+    }];
 
-    items2: string[] = [""];
+    newColumnTitle: string = "";
 
-    items1Column: string = "items1";
-    
-    items2Column: string = "items2";
+    addNewColumn() {
+      if(this.newColumnTitle === "") {
+        alert("New column needs a name!");
+      }
+      else {
+        let index: number = this.columns.length + 1;
+        this.columns.push({items: [""], columnKey: "items" + index, columnTitle: this.newColumnTitle});
+        this.saveToLocalStorage();
+        this.newColumnTitle = "";
+      }
+    }
+
+    removeColumn(index: number) {
+      this.columns.splice(index, 1);
+      this.saveToLocalStorage();
+    }
+
+    saveToLocalStorage() { 
+      // convert array to JSON string using JSON.stringify() 
+      const jsonArr = JSON.stringify(this.columns);
+  
+      // save to localStorage
+      localStorage.setItem("column", jsonArr);
+    }
+
+    ngOnInit(): void {
+      // get the string from localStorage
+      const str = localStorage.getItem("column");
+      if(str) {
+        // convert string to valid object
+        this.columns = JSON.parse(str);
+      }
+    }
 }
